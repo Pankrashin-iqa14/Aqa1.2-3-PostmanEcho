5d0bac6981a97f713c07ff43ff5c08ebc018ed74
Pankrashin-iqa14 <pankrashin.al@yandex.ru>
2021-04-05 19:49:05 +0400

    new commit

--- src/test/java/ru/netology/rest/PostmanEcho.java
+++ src/test/java/ru/netology/rest/PostmanEcho.java
@@ -21,7 +21,7 @@ class PostmanEcho {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"))
+                .body("data", equalTo("som data"))
         ;
     }
 }
